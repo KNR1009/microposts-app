948c5895c84c6ca5c38122c62d0b79cbeb95e398
伊東和成 <itokazunari5375@icloud.com>
2021-12-14 09:37:16 +0900

    add test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,11 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get root_path
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
+
 
   test "should get help" do
     get help_path

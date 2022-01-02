9ece4c93e572063642f1a6fc094c9e797beb5fde
伊東和成 <itokazunari5375@icloud.com>
2022-01-02 18:47:39 +0900

    ユーザーのshowメソットを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end

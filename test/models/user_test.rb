3a19e4648fed554fe27a8f32222c7a8153816b60
伊東和成 <itokazunari5375@icloud.com>
2022-01-02 17:25:23 +0900

    add sercure_password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
+  
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -33,20 +34,17 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-<<<<<<< HEAD
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-=======
-  
+  end
+
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
->>>>>>> effdc959ba16d9b97504652bda3a1d668d7ad1a3
   end
 end
